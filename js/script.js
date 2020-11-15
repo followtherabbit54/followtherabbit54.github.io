ba31c6d1a9d42536083fc3e656f4da38fd13f72e
Fernanda Penna <fernandapennaribeiro@gmail.com>
2020-11-15 18:16:02 -0300

    adding last media

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function myFunction() {
     var person = prompt("What are you wishing for?", "the answer must be simple");
     if (person == "you") {
         document.getElementById("demo").innerHTML =
-            "<a href='www.google.com'>www.google.com</a>";
+            "<a href='https://drive.google.com/drive/folders/1BwXSaiPDB8Radk6NAd974CCkZHQ8WTze?usp=sharing'>End of Transmition</a>";
         document.getElementById("image").src = "../img/inTimeQRcode.png"
     } else {
         document.getElementById("demo").innerHTML =
